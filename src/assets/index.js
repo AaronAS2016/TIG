bc44f2dc616b38cbdf3eb9a79274a1c65840a62b
= <aaron.saban@globant.com>
2020-05-30 13:13:35 -0300

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import logo from './logo.svg';
+import logo_white from './logo_white.svg';
+
+export const Logos = {
+    bg_black: logo,
+    bg_white: logo_white
+}
---
+++ src/main.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import styled, { createGlobalStyle } from "styled-components";
+import { Logos } from './assets';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300&display=swap');
+  
+
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+  html, body, #app {
+    height: 100%;
+    width: 100%;
+  }
+  
+  body {
+    font-family: Roboto;
+    font-weight: 300;
+  }
+`;
+
+const Logo = styled.img`
+  width: 50px;
+`;
+
+export const Main = () => {
+    return (
+        <>
+              <GlobalStyle />
+              <h1>Hola mundo</h1>
+              <Logo src={Logos.bg_black}/>
+        </>
+    )
+}
